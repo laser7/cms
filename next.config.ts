71acc98a6c09a521aba3834e9e5537e584df3f10
wenyanglei <wenyang.lei@balloon-events.com>
2025-09-09 13:33:03 +0800

    disable turbopack

--- next.config.ts
+++ next.config.ts
@@ -5,69 +5,72 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'fengmi-tarot.oss-cn-shenzhen.aliyuncs.com',
-        port: '',
-        pathname: '/yijing/**',
+        protocol: "https",
+        hostname: "fengmi-tarot.oss-cn-shenzhen.aliyuncs.com",
+        port: "",
+        pathname: "/yijing/**",
       },
       {
-        protocol: 'https',
-        hostname: 'example.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "example.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'www.haikei.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "www.haikei.co",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'www.krea.ai',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "www.krea.ai",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: '*.krea.ai',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "*.krea.ai",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'krea.ai',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "krea.ai",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: '*.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "*.cloudinary.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: '*.imgur.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "*.imgur.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: '*.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "*.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://dev.guara.fun/:path*',
+        source: "/api/:path*",
+        destination: "https://dev.guara.fun/:path*",
       },
-    ];
+    ]
   },
-};
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 export default nextConfig;
